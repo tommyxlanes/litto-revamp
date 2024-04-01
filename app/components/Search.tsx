04bf3167870ceaee697dd38f3f3af28ec83d346a
tommy <tommyvong88@gmail.com>
2024-03-31 18:25:23 -0700

    Change search bg

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -3,7 +3,7 @@ import { FaSearch } from 'react-icons/fa'
 
 const Search = () => {
   return (
-    <div className="w-full bg-white h-10 rounded-sm md:rounded-full flex justify-center lg:justify-between items-center">
+    <div className="w-full bg-white h-10 md:border-[1px] md:border-gray-300 rounded-sm md:rounded-full flex justify-center lg:justify-between items-center">
         <input type="text" className="bg-white ml-4 h-8 w-full outline-none" placeholder="Search"/>
         <div className="bg-white mr-2 rounded-full h-6 w-6 flex items-center 
         justify-center">
